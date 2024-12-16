40c9b9be9a2c6a55dbc713a08f3961a9553ff294
Aashish1-1-1 <aashishadhikari693@gmai.com>
2024-12-16 18:16:56 +0545

    Feat: Broadcast, H264 Impl

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rocket;
 use rocket::State;
 use std::sync::Arc;
 use webrtc::api::interceptor_registry::register_default_interceptors;
-use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_VP8};
+use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_H264};
 use webrtc::api::{APIBuilder, API};
 use webrtc::ice_transport::ice_connection_state::RTCIceConnectionState;
 use webrtc::ice_transport::ice_server::RTCIceServer;
@@ -11,23 +11,20 @@ use webrtc::interceptor::registry::Registry;
 use webrtc::peer_connection::configuration::RTCConfiguration;
 use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
 use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
-use webrtc::peer_connection::RTCPeerConnection;
+use webrtc::media::io::h264_reader::H264Reader;
 use webrtc::rtp_transceiver::rtp_codec::RTCRtpCodecCapability;
-use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
-use webrtc::track::track_local::{TrackLocal, TrackLocalWriter};
-use webrtc::util::Conn;
-
+use webrtc::track::track_local::TrackLocal;
+use webrtc::track::track_local::track_local_static_sample::TrackLocalStaticSample;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::{Request, Response};
+
+
 struct AetherWebRTCConnectionManager {
-    peer_connections: Vec<Arc<RTCPeerConnection>>,
-    screen_track: Option<Arc<TrackLocalStaticRTP>>,
+    screen_tracks: Arc<tokio::sync::Mutex<Vec<Arc<TrackLocalStaticSample>>>>,
     rtc_configuration: RTCConfiguration,
     api: API,
 }
-
-const LOCAL_RTP_ADDR: &str = "127.0.0.1:6421";
 
 impl AetherWebRTCConnectionManager {
     fn default() -> Option<Self> {
@@ -45,8 +42,7 @@ impl AetherWebRTCConnectionManager {
             .build();
 
         Some(Self {
-            peer_connections: vec![],
-            screen_track: None,
+            screen_tracks: Arc::new(tokio::sync::Mutex::new(vec![])),
             rtc_configuration: RTCConfiguration {
                 ice_servers: vec![RTCIceServer {
                     urls: vec!["stun:stun.l.google.com:19302".to_owned()],
@@ -62,23 +58,20 @@ impl AetherWebRTCConnectionManager {
         &mut self,
         offer: RTCSessionDescription,
     ) -> anyhow::Result<RTCSessionDescription> {
-        if let Some(screen_track) = &self.screen_track {
-            todo!("Implement broadcast system.")
-        } else {
-            let track = TrackLocalStaticRTP::new(
+        let screen_track = Arc::new(TrackLocalStaticSample::new(
                 RTCRtpCodecCapability {
-                    mime_type: MIME_TYPE_VP8.to_owned(),
+                    mime_type: MIME_TYPE_H264.to_owned(),
                     ..Default::default()
                 },
                 "video".to_owned(),
                 "aether-rtc-screen".to_owned(),
-            );
+            ));
 
-            self.screen_track = Some(Arc::new(track));
-        }
+            
+        self.screen_tracks.lock().await.push(Arc::clone(&screen_track));
+        let track_index = self.screen_tracks.lock().await.len() - 1;
+        
 
-        // At this point, screen_track cannot be expected to be None.
-        let track = self.screen_track.clone().unwrap();
 
         let peer = Arc::new(
             self.api
@@ -87,13 +80,9 @@ impl AetherWebRTCConnectionManager {
         );
 
         let rtp_sender = peer
-            .add_track(Arc::clone(&track) as Arc<dyn TrackLocal + Send + Sync>)
+            .add_track(Arc::clone(&screen_track) as Arc<dyn TrackLocal + Send + Sync>)
             .await?;
 
-        // TODO: Add this upon connection!
-        self.peer_connections.insert(0, Arc::clone(&peer));
-
-        // Handle incoming packets, if any.
         tokio::spawn(async move {
             let mut rtcp_buf = vec![0u8; 1500];
             while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
@@ -102,8 +91,10 @@ impl AetherWebRTCConnectionManager {
 
         let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
         let done_tx1 = done_tx.clone();
-        let done_tx2 = done_tx.clone();
-        let done_tx3 = done_tx.clone();
+
+
+        let notify = Arc::new(tokio::sync::Notify::new());
+        let notify2 = Arc::clone(&notify);
 
         peer.on_ice_connection_state_change(Box::new(
             move |connection_state: RTCIceConnectionState| {
@@ -112,6 +103,9 @@ impl AetherWebRTCConnectionManager {
                     | RTCIceConnectionState::Disconnected
                     | RTCIceConnectionState::Closed => {
                         let _ = done_tx1.try_send(());
+                    }
+                    RTCIceConnectionState::Connected => {
+                        notify.notify_one();
                     }
                     _ => {}
                 }
@@ -126,7 +120,7 @@ impl AetherWebRTCConnectionManager {
                     RTCPeerConnectionState::Failed
                     | RTCPeerConnectionState::Disconnected
                     | RTCPeerConnectionState::Closed => {
-                        let _ = done_tx2.try_send(());
+                        let _ = done_tx.try_send(());
                     }
                     _ => {}
                 }
@@ -137,78 +131,89 @@ impl AetherWebRTCConnectionManager {
 
         peer.set_remote_description(offer).await?;
 
+
+
         let answer = peer.create_answer(None).await?;
         let mut gather_complete = peer.gathering_complete_promise().await;
 
         peer.set_local_description(answer).await?;
         let _ = gather_complete.recv().await;
 
-        // We expect a local description to be set here.
-        // Thereforce, we safely unwrap.
         let answer = peer.local_description().await.unwrap();
 
-        // Currently, this part gets executed for each peer which may be
-        // detrimental to the server performance.
+        let screen_tracks = Arc::clone(&self.screen_tracks);
+        let screen_tracks_2 = Arc::clone(&self.screen_tracks);
 
-        // Not to mention, this runs irrespective of connection.
-
-        // In future, when broadcast is implemented, this part is expected
-        // to be isolated and the transmissions to be closed when there are
-        // are no more peer connections.
-        tokio::spawn(async move {
-            let listener = Arc::new(tokio::net::UdpSocket::bind(LOCAL_RTP_ADDR).await.unwrap());
-            let listener_1 = Arc::clone(&listener);
-
-            let mut ffmpeg_process = tokio::process::Command::new("ffmpeg")
-                .args(vec![
-                    "-hide_banner",
-                    "-loglevel",
-                    "error",
-                    "-f",
-                    "gdigrab",
-                    "-i",
-                    "desktop",
-                    "-vf",
-                    "scale=1280:720",
-                    "-r",
-                    "24",
-                    "-pix_fmt",
-                    "yuv420p",
-                    "-c:v",
-                    "libvpx",
-                    "-b:v",
-                    "2M",
-                    "-f",
-                    "rtp",
-                    &format!("rtp://{LOCAL_RTP_ADDR}?pkt_size=1200"),
-                ])
-                .spawn()
-                .unwrap();
-
+        if self.screen_tracks.lock().await.len() == 1 {
             tokio::spawn(async move {
-                let mut inbound_rtp_packet = vec![0u8; 1600];
-                while let Ok((n, _)) = listener_1.recv_from(&mut inbound_rtp_packet).await {
-                    if let Err(err) = track.write(&inbound_rtp_packet[..n]).await {
-                        if webrtc::Error::ErrClosedPipe == err {
-                            // Connection is closed.
-                        } else {
-                            // Some other error, yet to handle.
-                        }
-                        let _ = done_tx3.try_send(());
-                        return;
-                    }
-                }
-            });
+                notify2.notified().await;
 
-            tokio::select! {
-                _ = done_rx.recv() => {
-                    eprintln!("Closing peer connection due to disconnection or failure signal.");
-                    let _ = listener.close().await;
-                    let _ = ffmpeg_process.start_kill();
-                    let _ = peer.close().await;
-                }
-            };
-        });
+                tokio::spawn(async move {
+                    let ffmpeg_process = std::process::Command::new("ffmpeg")
+                    .args(vec![
+                        "-re",
+                        "-device",
+                        "/dev/dri/card1",
+                        "-f",
+                        "kmsgrab",
+                        "-i",
+                        "-",
+                        "-vf",
+                        "hwmap=derive_device=vaapi,scale_vaapi=w=1280:h=720:format=nv12",
+                        "-c:v",
+                        "h264_vaapi",
+                        "-bsf:v",
+                        "h264_mp4toannexb",
+                        "-r",
+                        "24",
+                        "-b:v",
+                        "1M",
+                        "-f",
+                        "h264",
+                        "-"
+                    ])
+                    .stdout(std::process::Stdio::piped())
+                    .stderr(std::process::Stdio::null())
+                    .spawn()
+                    .unwrap();
+
+                    let stdout = ffmpeg_process.stdout.unwrap();
+                    let mut h264_source = H264Reader::new(stdout, 1_048_576);
+
+                    let mut ticker = tokio::time::interval(std::time::Duration::from_millis(33));
+
+                    while let Ok(nal) = h264_source.next_nal() {
+                        let sample = webrtc::media::Sample {
+                            data: nal.data.freeze(),
+                            duration: std::time::Duration::from_secs(1),
+                            ..Default::default()
+                        };
+                        
+                        for track in screen_tracks.lock().await.iter() {
+                            if track.write_sample(&sample).await.is_err() {
+                                break
+                            }
+                        }
+
+                        let _ = ticker.tick().await;
+
+                        if screen_tracks.lock().await.is_empty() {
+                            break;
+                        }
+
+                    }
+                });
+
+                
+            });
+        }
+
+        tokio::spawn(async move {tokio::select! {
+            _ = done_rx.recv() => {
+                let _ = peer.close().await;
+                screen_tracks_2.lock().await.remove(track_index);
+            }
+        };});
 
         anyhow::Ok(answer)
     }
